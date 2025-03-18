0a721cb6e1201b4b82b9bf0cc6ed98d0af30f961
DanyelleH <danyellehenriquez@gmail.com>
2025-03-17 22:16:25 -0400

    Pages prepared for auth, Auth endpoints added to api folder

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,64 @@
-import React from "react"
-
+import React, {useState} from "react"
+import { useNavigate } from 'react-router-dom';
+import { Paper, Box, TextField } from "@mui/material"
+import Button from '@mui/material/Button';
 export default function LoginPage() {
+    const [loading, setLoading]=useState(false)
+    const navigate=useNavigate()
+    const handleLogin = () => {
+        return (
+            setLoading(true),
+            console.log("logging in")
+        )
+    }
+    const handleNewUser = () => {
+        return(
+            navigate("/signup")
+        )
+    }
+
     return (
-    <p>This is the login page</p>
+        <Paper
+        square={false}
+        elevation={4}>
+
+            <h3>Login</h3>
+            <Box
+                component="form"
+                sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+                noValidate
+                autoComplete="off"
+                >
+                    <div>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Username"
+                        placeholder="Enter username"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Password"
+                        placeholder="Enter password"
+                        />
+                    </div>
+                    <div>
+                    <Button
+                        onClick={handleLogin}
+                        // endIcon={<SendIcon />}
+                        loading={loading}
+                        loadingPosition="end"
+                        variant="contained"
+                        >
+                        Submit
+                    </Button>
+                    </div>
+            </Box>
+            <p> Need an account?</p>
+            <Button onClick={handleNewUser}>Create an Account </Button>
+        </Paper>
     )
 }
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,61 @@
-import React from "react"
+import React, {useState} from "react"
+import { useNavigate } from 'react-router-dom';
+import { Paper, Box, TextField } from "@mui/material"
+import Button from '@mui/material/Button';
 export default function SignUp () {
+    const [loading, setLoading]=useState(false)
+    const navigate = useNavigate()
+
+    const handleNavigate = () => {
+       navigate("/login")
+    }
+
+    const handleSignUp = () => {
+        setLoading(true)
+        console.log("signing up new user")
+    }
+
     return (
-        <p> Signup Page</p>
+        <Paper
+        square={false}
+        elevation={4}>
+
+            <h3>SignUp</h3>
+            <Box
+                component="form"
+                sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+                noValidate
+                autoComplete="off"
+                >
+                    <div>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Username"
+                        placeholder="Enter username"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Password"
+                        placeholder="Enter password"
+                        />
+                    </div>
+                    <div>
+                    <Button
+                        onClick={handleSignUp}
+                        loading={loading}
+                        loadingPosition="end"
+                        variant="contained"
+                        >
+                        Submit
+                    </Button>
+                    </div>
+                    <p> Have an account?</p>
+            <Button onClick={handleNavigate}> Sign In </Button>
+            </Box>
+        </Paper>
     )
 }
